5e41914e5c8880190b646c77eecc1dfa595c65fc
Muhammad Oka <oka@muhammadoka.dev>
2024-02-02 10:21:00 +0700

    auto increment product id

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -14,8 +14,12 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    private int id = 0;
+
     @Override
     public Product create(Product product) {
+        product.setProductId(Integer.toString(++id));
+
         productRepository.create(product);
         return product;
     }
