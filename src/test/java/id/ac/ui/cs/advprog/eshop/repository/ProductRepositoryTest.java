14bb6e4a2415aeaec0eaa11fdf673b295a632c05
Muhammad Oka <oka@muhammadoka.dev>
2024-02-08 16:37:54 +0700

    feat: modify edit product test case to include all branches

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -120,15 +120,25 @@ class ProductRepositoryTest {
         product2.setProductId("a0f9de46-90b1-437d-a0bf-d0821dde9096");
         product2.setProductName("Sampo Cap Usep");
         product2.setProductQuantity(50);
+        productRepository.create(product2);
+
+        Product product3 = new Product();
+        product3.setProductId("106fc3c2-530d-481b-a82c-9f56befbab8c");
+        product3.setProductName("Sampo Cap Capan");
+        product3.setProductQuantity(75);
 
         Iterator<Product> productIterator = productRepository.findAll();
-        productRepository.edit(product1.getProductId(), product2);
+        assertNotNull(productRepository.edit(product2.getProductId(), product3));
 
         Product savedProduct = productIterator.next();
-        assertEquals(savedProduct.getProductId(), product1.getProductId());
-        assertEquals(savedProduct.getProductId(), product2.getProductId());
-        assertEquals(savedProduct.getProductName(), product2.getProductName());
-        assertEquals(savedProduct.getProductQuantity(), product2.getProductQuantity());
+        assertEquals(product1.getProductId(), savedProduct.getProductId());
+        assertEquals(product1.getProductName(), savedProduct.getProductName());
+        assertEquals(product1.getProductQuantity(), savedProduct.getProductQuantity());
+
+        savedProduct = productIterator.next();
+        assertEquals(product2.getProductId(), savedProduct.getProductId());
+        assertEquals(product3.getProductName(), savedProduct.getProductName());
+        assertEquals(product3.getProductQuantity(), savedProduct.getProductQuantity());
     }
 
     @Test
